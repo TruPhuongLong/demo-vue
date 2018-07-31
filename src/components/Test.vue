f7090f73e2c352d7cdb829df4d6be7a2845ce1ca
blsmac1 <blsmac20171@gmail.com>
2018-07-31 09:20:51 +0700

    transition element, name , key

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,20 +1,35 @@
 <template>
-    <div>
+    <div class="container">
         Test {{array}}
         <!-- <ul>
             <li v-for="e, index in array" >{{e}} - {{index}}</li>
         </ul>    -->
-        <button v-on:click="toogle">{{'click me' | capital}}</button>
+        <button @click="toogle">{{'click me' | capital}}</button>
 
-        <transition name="toggle">
+        <!-- <transition name="toggle">
             <p v-show="show">toogle</p>
-        </transition>
+        </transition> -->
 
         <hr />
         filter array:
         <ul>
-            <li v-for="e in even(array)">{{e}}</li>
+            <!-- <li v-for="e in even(array)">{{e}}</li> -->
         </ul>
+
+        <hr />
+        transition many element:
+        <!-- <div >
+        <transition name="oppacity" >
+            <p v-if="show" key="1">element 1</p>
+            <p v-if="show == false" key="2">element 2</p>
+        </transition>
+        </div> -->
+        <hr />
+        <div >
+            <transition name="slide" >
+                <p :key="tran[index%(tran.length)]">{{tran[index%(tran.length)]}}</p>
+            </transition>
+        </div>
         
 
 
@@ -24,37 +39,40 @@
 
 <script>
 export default {
-    data(){
-        return {
-            array: [1,5,6, 4, 8],
-            side: 10,
-            show: true
-        }
-    },
+  data() {
+    return {
+      array: [1, 5, 6, 4, 8],
+      side: 10,
+      show: true,
+      index: 0,
+      tran: ["element 1", "what are you doing", "hiiiiii"]
+    };
+  },
 
-    computed: {
-        square(){
-            return this.side * this.side;
-        }
-    },
-
-    methods: {
-        toogle(){
-            console.log('toogle')
-            if(this.show)
-                this.show = false;
-            else this.show = true;
-        },
-
-        even(array){
-            return array.filter(e => e % 2 == 0);
-        }
+  computed: {
+    square() {
+      return this.side * this.side;
     }
+  },
 
-    // data: {
-    //     array: [1,5,6]
-    // }
-}
+  methods: {
+    toogle() {
+      console.log("toogle");
+      if (this.show) this.show = false;
+      else this.show = true;
+
+      this.index++;
+    },
+
+    even(array) {
+      return array.filter(e => e % 2 == 0);
+    }
+  }
+
+  // data: {
+  //     array: [1,5,6]
+  // }
+};
 </script>
 
 
@@ -62,13 +80,47 @@ export default {
 
 
 <style>
-    .toggle-enter{
-        /* opacity: 0; */
-        transform: translateX(200px);
-    }
+.toggle-enter {
+  /* opacity: 0; */
+  transform: translateX(200px);
+}
 
-    .toggle-enter-active{
-        transition: all 2s ease-in;
-    }
+.toggle-enter-active {
+  transition: all 2s ease-in-out;
+}
+
+.oppacity-enter,
+.oppacity-leave {
+  opacity: 0;
+}
+.oppacity-enter-active,
+.oppacity-leave-active {
+  transition: 1s;
+}
+
+.slide-enter{
+    transform: translateX(200px);
+}
+
+.slide-enter-active{
+    transition: 1s;
+}
+
+.slide-leave-active{
+    transform: translateX(-200px)
+}
+
+p {
+  margin: 0;
+  position: absolute;
+  font-size: 3em;
+}
+
+.container{
+    width: 80%;
+    margin: auto;
+}
+
+
 </style>
 
