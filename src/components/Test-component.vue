403901d163c4371f20d540d6935c604a2b4e0c2d
blsmac1 <blsmac20171@gmail.com>
2018-07-31 15:13:28 +0700

    component : use template or createElement

---
+++ src/components/Test-component.vue
@@ -0,0 +1,26 @@
+
+
+<template>
+<div>
+    <h1>hi chapter 4- component</h1>
+ 
+    <hi name="23fsfsfs"></hi>
+
+    <r></r>
+</div>
+</template>
+
+
+<script>
+export default {
+    
+}
+
+</script>
+
+
+<style>
+
+</style>
+
+
--- src/main.js
+++ src/main.js
@@ -2,13 +2,40 @@ import Vue from 'vue'
 import App from './App.vue'
 import Label from './components/Label.vue'
 import Test from './components/Test'
-
-new Vue({
-  el: '#app',
-  render: h => h(Test)
-})
-
+import Com from './components/Test-component.vue'
 
 Vue.filter('capital', string => {
   return string.toUpperCase();
 })
+
+// Define a new component called todo-item
+Vue.component('hi', {
+  template: `
+    <div>
+      <h1>welcome here {{name}}</h1>
+    </div>
+  `,
+  // props: ['name']
+  // props: {
+  //   name: Number
+  // }
+  props: {
+    name: [Number, String]
+  }
+})
+
+//component 
+Vue.component('r', {
+  functional: true,
+  render: h => h('p', 'hi im new')
+})
+
+new Vue({
+  el: '#app',
+  render: h => h(Com)
+})
+
+
+
+
+
